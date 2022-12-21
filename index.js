acb3f81b21c9c9af0d7b32b4d16764e6ca14b7a6
arifwdn <muhammadarifwardana01@gmail.com>
2022-12-21 21:35:54 +0800

    Benerin Validasi

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const N = 5
-const firstArr = [1,0,1,1,0]
+const firstArr = [1,0,1,0,1]
 const secArr = [50,65,55,45,30]
 
 const findMinimum = (n, a, b)=>{
     // Validator
-    if(a.length > n || b.length > n){
-        return -1
+    if(a.length > n || b.length > n || a.length < n || b.length < n){
+        return 0
     }
     const checkA = a.find(data => data<0 || data>1)
     const checkB = b.find(data => data<1 || data>1000000)
     if(checkA != undefined || checkB != undefined){
-        return -1
+        return 0
     }
 
     //Solution
